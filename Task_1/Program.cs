ee299ad35f65b36d2a78c10668cec6f7b26c5255
Dima Ivanov <ivanovdimat5@gmail.com>
2023-01-21 20:54:25 +0300

    Добавлены решение первой задачи

---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.Clear();
+
+int GetNumberByUser(string text)
+{
+    Console.Write(text + "  ");
+    return int.Parse(Console.ReadLine());
+}
+
+void NumberCounter (int number)
+{
+    if (number == 0) return;
+    Console.Write($" {number}");
+    NumberCounter (number - 1);
+
+}   
+
+int number = GetNumberByUser("Введите число больше 0:" );
+NumberCounter(number);
